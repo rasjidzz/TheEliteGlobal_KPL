0ee11d75e76e17e0332db283eb56f4b69601935d
Foxxie1440 <90520474+ZhafranHafizh@users.noreply.github.com>
2023-05-08 17:14:29 +0700

    Home Page

---
+++ EliteGlobal_homepage/Form1.cs
@@ -0,0 +1,25 @@
+namespace EliteGlobal_homepage
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click_1(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
